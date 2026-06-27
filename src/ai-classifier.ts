aa5a000fef34b1aeaccdf3910e663d4c22d6dfde
danikkks <hide.my.email939@icloud.com>
2026-06-27 20:02:19 +0200

    move ai classifier into separate module

---
+++ src/ai-classifier.ts
@@ -0,0 +1,32 @@
+import { OpenAI } from "openai";
+import { Ollama } from "ollama/dist/index.cjs";
+import { stripHtml } from "string-strip-html";
+import { type IAIClassifier } from "./core.js";
+
+export const makeAIClassifier = (): IAIClassifier => {
+    if (process.env.AI_PROVIDER === "OPENAI") {
+        const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || "" });
+        return {
+            classify: async (entry, prompt) => {
+                const r = await client.responses.create({
+                    model: "gpt-5-nano",
+                    instructions: prompt,
+                    input: `# ${entry.title}\n${entry.content}`,
+                });
+                return r.output_text;
+            },
+        };
+    }
+
+    const client = new Ollama({ host: process.env.OLLAMA_BASE_URL });
+    return {
+        classify: async (entry, prompt) => {
+            const r = await client.generate({
+                model: process.env.OLLAMA_MODEL,
+                prompt: `${prompt}\n\n ${stripHtml(entry.title).result}\n${entry.content.length > 1000 ? "" : stripHtml(entry.content).result}`,
+                think: true,
+            });
+            return r.response;
+        },
+    };
+};
--- src/miniflux-ai.ts
+++ src/miniflux-ai.ts
@@ -1,37 +1,7 @@
-import { OpenAI } from "openai";
-import { Ollama } from "ollama/dist/index.cjs";
-import { stripHtml } from "string-strip-html";
-import { type IAIClassifier, run } from "./core.js";
+import { run } from "./core.js";
 import { makeMinifluxClient } from "./miniflux.js";
 import { promptLoader } from "./prompt-loader.js";
-
-const makeAIClassifier = (): IAIClassifier => {
-    if (process.env.AI_PROVIDER === "OPENAI") {
-        const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || "" });
-        return {
-            classify: async (entry, prompt) => {
-                const r = await client.responses.create({
-                    model: "gpt-5-nano",
-                    instructions: prompt,
-                    input: `# ${entry.title}\n${entry.content}`,
-                });
-                return r.output_text;
-            },
-        };
-    }
-
-    const client = new Ollama({ host: process.env.OLLAMA_BASE_URL });
-    return {
-        classify: async (entry, prompt) => {
-            const r = await client.generate({
-                model: process.env.OLLAMA_MODEL,
-                prompt: `${prompt}\n\n ${stripHtml(entry.title).result}\n${entry.content.length > 1000 ? "" : stripHtml(entry.content).result}`,
-                think: true,
-            });
-            return r.response;
-        },
-    };
-};
+import { makeAIClassifier } from "./ai-classifier.js";
 
 (async () => {
     const miniflux = makeMinifluxClient(
